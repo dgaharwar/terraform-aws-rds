1e543647c42b45c9ab389789cce8431f71c4c3eb
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-10-28 15:10:15 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,12 +37,12 @@ variable "apply_immediately" {
 variable "security_group_ids" {
   description = "Comma-separated list of security group IDs"
   type        = string
-  default     = "sg-12345678,sg-87654321"
+  default     = "[{\"id\":\"sg-12345678\"},{\"id\":\"sg-87654321\"}]"
 }
 
 # Local variable to transform the security group IDs
 locals {
-  security_groups = split(",", var.security_group_ids)
+  security_groups = [for sg in jsondecode(var.security_group_ids) : sg.id]
 }
 
 # Local variable to split the security group IDs
