fe21c48cb25aced814d335cdb8c2701fa5d0f718
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-10-28 11:12:46 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ resource "aws_db_instance" "default" {
   vpc_security_group_ids    = local.security_groups
   allocated_storage         = var.allocated
   max_allocated_storage     = var.max_allocated
-  storage_type              = "gp2"
+  storage_type              = "gp3"
   engine                    = "mysql"
   engine_version            = var.engine_version
-  instance_class            = "db.t2.micro"
-  parameter_group_name      = "default.mysql5.7"
+  instance_class            = "db.t4g.micro"
+  parameter_group_name      = "default.mysql8.0"
   apply_immediately         = var.apply_immediately
   final_snapshot_identifier = "mysql-backup"
   skip_final_snapshot       = true
