d5075a4179c154d077d69537533ce249e4ee1e48
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-10-28 10:09:42 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,50 @@
+################################
+##         Variables          ##
+################################
+
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "allocated" {
+    description = "DB Size in GB"
+    default     = "20"
+}
+
+variable "max_allocated" {
+    description = "Max Autogrow DB Size in GB"
+    default     = "100"
+}
+
+variable "engine_version" {
+    description = "Version of the RDS Engine"
+    default     = "5.7"
+}
+
+variable "db_user" {
+    description = "DB User"
+    default     = "admin"
+}
+
+variable "db_password" {
+    description = "DB Password"
+    sensitive   = true
+    default     = "<%=cypher.read('secret/rds')%>"
+}
+
+variable "apply_immediately" {
+    description = "Apply Changes Immediately"
+    default     = "true"
+}
+
+# Variable for security group IDs
+variable "security_group_ids" {
+  description = "Comma-separated list of security group IDs"
+  type        = string
+  default     = "sg-12345678,sg-87654321"
+}
+
+# Local variable to split the security group IDs
+locals {
+  security_groups = split(",", var.security_group_ids)
+}
